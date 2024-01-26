0c0c844cd85851351e1a59bb5fbc44fa4a7b335f
Vinicius Kalach <viniciuscato@hotmail.com>
2024-01-26 00:01:54 -0300

    feat: create showcase page and connect to login page

---
+++ src/pages/ShowcasePage.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import "../styles/Showcase.css";
+import filmMarcoAurelio from "../assets/films/filmMarcoAurelio.jpg";
+import filmTheChosen from "../assets/films/filmTheChosen.jpg";
+import filmPeakyBlinders from "../assets/films/filmPeakyBlinders.jpg";
+import filmHalloween from "../assets/films/filmHalloween.png";
+import filmDostoiévski from "../assets/films/filmDostoiévski.jpg";
+import filmTolstoi from "../assets/films/filmTolstói.jpg";
+import filmMatrix from "../assets/films/filmMatrix.png";
+import filmPiratasDoCaribe from "../assets/films/filmPiratasDoCaribe.jpg";
+import filmOHobbit from "../assets/films/filmOHobbit.jpg";
+import filmOHobbit2 from "../assets/films/filmOHobbit2.jpg";
+import filmOHobbit3 from "../assets/films/filmOHobbit3.jpg";
+import filmSenhorDosAneis from "../assets/films/filmSenhorDosAneis.jpg";
+import filmSenhorDosAneis2 from "../assets/films/filmSenhorDosAneis2.jpg";
+import filmSenhorDosAneis3 from "../assets/films/filmSenhorDosAneis3.jpg";
+import filmPlatao from "../assets/films/filmPlatao.jpg";
+import { Link } from "react-router-dom";
+
+const App: React.FC = () => {
+  const linkedin =
+    "https://www.linkedin.com/in/vinícius-kalach-freitas-cato-a91951192";
+
+  return (
+    <>
+      <div className="HeaderShowcase">
+        <h1>NETFLEX</h1>
+        <Link to="/login">Sair</Link>
+      </div>
+
+      <div className="wrapper">
+        <section id="section1">
+          <a href="#section3">‹</a>
+          <div className="item">
+            <img src={filmPeakyBlinders} alt="série Peaky Blinders" />
+          </div>
+          <div className="item">
+            <img src={filmMarcoAurelio} alt="filme do Marco Aurélio" />
+          </div>
+          <div className="item">
+            <img src={filmTheChosen} alt="série The Chosen" />
+          </div>
+          <div className="item">
+            <img src={filmHalloween} alt="filme do Halloween" />
+          </div>
+          <div className="item">
+            <img src={filmDostoiévski} alt="filme do Dostoiévski" />
+          </div>
+          <a href="#section3">›</a>
+        </section>
+        <section id="section2">
+          <a href="#section1">‹</a>
+          <div className="item">
+            <img src={filmMatrix} alt="filme Matrix" />
+          </div>
+          <div className="item">
+            <img src={filmTolstoi} alt="filme do Tolstói" />
+          </div>
+          <div className="item">
+            <img src={filmPiratasDoCaribe} alt="filme Piratas do Caribe" />
+          </div>
+          <div className="item">
+            <img src={filmPlatao} alt="filme do Platão" />
+          </div>
+          <div className="item">
+            <img src={filmSenhorDosAneis} alt="filme Senhor dos Anéis 1" />
+          </div>
+          <a href="#section3">›</a>
+        </section>
+
+        <section id="section3">
+          <a href="#section2">‹</a>
+          <div className="item">
+            <img src={filmSenhorDosAneis2} alt="filme Senhor dos Anéis 2" />
+          </div>
+          <div className="item">
+            <img src={filmSenhorDosAneis3} alt="filme Senhor dos Anéis 3" />
+          </div>
+          <div className="item">
+            <img src={filmOHobbit} alt="filme O Hobbit 1" />
+          </div>
+          <div className="item">
+            <img src={filmOHobbit2} alt="filme O Hobbit 2" />
+          </div>
+          <div className="item">
+            <img src={filmOHobbit3} alt="filme O Hobbit 3" />
+          </div>
+          <a href="#section1">›</a>
+        </section>
+      </div>
+
+      <footer className="footer-container">
+        <div className="footer-content">
+          <div className="contact">
+            <h3>Contato</h3>
+            <p>
+              viniciuscato@hotmail.com |{" "}
+              <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                LinkedIn
+              </a>
+            </p>
+          </div>
+        </div>
+
+        <div className="about-us">
+          <h3>Sobre Nós</h3>
+          <p>Netflex - Sua plataforma de streaming favorita.</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,17 @@
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './pages/LoginPage';
+import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ShowcasePage from './pages/ShowcasePage';
 import './App.css'
 
 function RoutesComponent() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/registro" element={<RegisterPage />} />
+        <Route path="/vitrine" element={<ShowcasePage />} />
         <Route path='*' element={<h1>Not found</h1>}/>
       </Routes>
     </BrowserRouter>
